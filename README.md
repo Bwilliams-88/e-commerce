5eb0b73885b07c067762645e2824ad6795d7ec55
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-09-11 19:24:12 -0500

    all files finished, just need to test it with insomnia and upload video / input links into README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# E-commerce Back End Starter Code
+# E-commerce Project
+## Putting this project code together was pretty easy (I didn't think it would be since I have been having alot of issues with MySQL) for me when I referred back to the lessons that we covered in our live classes. I am feeling pretty confident in this assignment which is a huge relief from the assignment last week.
+
+# LINKS
+## Video Demonstration:
+## Repo:
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,12 +8,28 @@ GET /api/Product
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Tag }, { model: Category}],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      included: [{ model: Tag }, { model: Category}],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -26,7 +42,7 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -93,8 +109,24 @@ router.put('/:id', async (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
